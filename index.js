3441e95e434f71dad37eb25ef713e05f3619590b
Amanda Sousa Bezerra <amanda.sousabez@gmail.com>
2021-03-25 10:53:18 -0300

    add lista de pets

---
+++ index.js
@@ -0,0 +1,48 @@
+const nomePetshp = 'Petshop Avanade';
+
+let pets = [
+    {
+        nome: 'Orion',
+        tipo: 'cachorro',
+        idade: 5,
+        raca: 'Pastor Australiano',
+        peso: 10,
+        tutor: 'Tom',
+        contato: '(81) 98746-0809',
+        vacinado: true,
+        servicos: ['banho', 'tosa']    
+    },
+    {
+        nome: 'Scooby-Doo',
+        tipo: 'cachorro',
+        idade: 3,
+        raca: 'Dogue Alemão',
+        peso: 50,
+        tutor: 'Salsicha',
+        contato: '(11) 95670-1029',
+        vacinado: true,
+        servicos: ['banho']    
+    },
+    {
+        nome: 'Salem',
+        tipo: 'gato',
+        idade: 10,
+        raca: 'Vira-Lata',
+        peso: 2.5,
+        tutor: 'Sabrina',
+        contato: '(81) 97597-5257',
+        vacinado: true,
+        servicos: ['banho', 'corte de unhas']    
+    }
+];
+
+//Usando Arrow Functions:
+
+const listarPets = () => {
+    for(let i=0; i < pets.length; i++)
+        console.log(pets[i].nome);
+}
+
+listarPets();
+
+// console.log(pets);
